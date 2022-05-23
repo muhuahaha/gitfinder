11f56d3d35b288b7682ead6276f8b0404ae87920
Young <kimyou2019@gmail.com>
2022-05-23 08:08:03 +0200

    add axios

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,19 +3,32 @@ import { useSelector, useDispatch } from 'react-redux';
 import NavbarTest from '../components/layout/NavbarTest';
 import { Counter } from '../features/counter/Counter';
 import CartContainer from '../components/cart/CartContainer';
-import { calculateTotals } from '../features/cart/cartSlice';
+import { calculateTotals, getCartItems } from '../features/cart/cartSlice';
 import Modal from '../components/layout/Modal';
 
 function About() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
 
   console.log(isOpen, 'isOpen');
 
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <div>
