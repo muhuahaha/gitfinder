cf6125d16053913c2ba2fbdc55eaf611c3e7c57b
Young <kimyou2019@gmail.com>
2022-05-24 23:22:44 +0200

    add repos

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -14,12 +14,13 @@ console.log(GITHUB_URL);
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users (test purposes)
   const searchUsers = async (text) => {
     setLoading();
 
@@ -40,6 +41,52 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = '/notfound';
+    } else {
+      console.log(response, 'response');
+
+      const data = await response.json();
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      });
+    }
+  };
+
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({ sort: 'create', per_page: 10 });
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    console.log(response, 'response Repos');
+
+    const data = await response.json();
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    });
+  };
+
   // Clear users form this.state.
   const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
 
@@ -49,8 +96,12 @@ export const GithubProvider = ({ children }) => {
   const value = useMemo(() => ({
     users: state.users,
     loading: state.loading,
+    user: state.user,
+    repos: state.repos,
     searchUsers,
     clearUsers,
+    getUser,
+    getUserRepos,
   }));
 
   return <GithubContext.Provider value={value}>{children}</GithubContext.Provider>;
